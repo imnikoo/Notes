5070af4f11a3127c712739d2d136c804d6a3859c
imnikoo <efruev@gmail.com>
2017-11-26 23:44:31 +0200

    Added some animations

--- src/containers/start/index.js
+++ src/containers/start/index.js
@@ -1,35 +1,63 @@
-import React from 'react'
-import {push} from 'react-router-redux'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
-import './styles.css';
+import React, {Component} from "react";
+import {goBack, push} from "react-router-redux";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import "./styles.css";
+import {CSSTransition} from "react-transition-group";
 
-const Start = props => (
-   <div className="landing-container">
-      <div className="introduction">
-         <span className="introduction__app-name">Notes</span>
-         <span className="app-description">
+class Start extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({show: true});
+    }
+
+    render() {
+        return (
+            <FadeIn in={this.state.show}>
+                <div className="landing-container">
+                    <div className="introduction">
+                        <span className="introduction__app-name">Notes</span>
+                        <span className="app-description">
                Welcome to the <span className="app-description__app-name">Notes</span>.
                This application helps you to stay on a short leg with your notes, information you need to save and use.
                You can create notes, change it, open whenever you want.
                <br/><span className="app-description__app-name">Notes</span> is Future.
             </span>
-      </div>
-      <div className="actions">
-         <div className="actions__sign-up" onClick={props.registrationPage}/>
-         <div className="actions__sign-in" onClick={props.logInPage}/>
-      </div>
-   </div>
-);
+                    </div>
+                    <div className="actions">
+                        <div className="actions__sign-up" onClick={this.props.registrationPage}/>
+                        <div className="actions__sign-in" onClick={this.props.logInPage}/>
+                    </div>
+                </div>
+            </FadeIn>)
+    }
+}
+
+const FadeIn = ({children, ...props}) => {
+    return <CSSTransition
+        {...props}
+        timeout={1000}
+        classNames="fade-in">
+        {children}
+    </CSSTransition>
+};
 
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-   registrationPage: () => push('/registration'),
-   logInPage: () => push('/login')
+    goBack,
+    registrationPage: () => push('/registration'),
+    logInPage: () => push('/login')
 }, dispatch);
 
 export default connect(
-   mapStateToProps,
-   mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Start)
